762bd57e5b5bd95a08b09d1c87b34007fe4763fc
masan3134 <masan3134@users.noreply.github.com>
2025-07-20 14:01:05 +0300

    Auto init project

---
+++ app/src/main/java/com/example/webview/MainActivity.kt
@@ -0,0 +1,15 @@
+package com.example.webview
+
+import android.os.Bundle
+import android.webkit.WebView
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val webView = WebView(this)
+        setContentView(webView)
+        webView.settings.javaScriptEnabled = true
+        webView.loadUrl("https://es-exchange.com/")
+    }
+}
